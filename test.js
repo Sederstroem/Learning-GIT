d207e3a0af899426d56472efd98f25a924c60a66
--global <christian.sederstrom@gmail.com>
2023-02-27 18:07:33 +0200

    Funkar det FF?

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 function add(num1, num2, num3) {
     return num1 + num2 + num3;
 }
+
+// Lägger till en ny funktion
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
--- test2.js
+++ test2.js
@@ -4,3 +4,5 @@
 
 
 //TJUUU
+
+// KOM IGEN NU
