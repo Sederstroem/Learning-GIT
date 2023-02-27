a0c25012b072912fca371bf2829e77afe60b7a93
Sederstroem <125889848+Sederstroem@users.noreply.github.com>
2023-02-27 18:08:52 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,4 @@ function add(num1, num2, num3) {
     return num1 + num2 + num3;
 }
 
-// Lägger till en ny funktion
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
+// TAR bort subtract()
